9ef6f958580b8b5aa539a83dce4e4b6ba0eb5a11
Davide Porrovecchio <pdavide@users.noreply.github.com>
2019-03-28 10:20:33 +0100

    Missing curly brace

--- conf.py
+++ conf.py
@@ -6,14 +6,11 @@
 latex_elements = {
 # The paper size ('letterpaper' or 'a4paper').
 'papersize': 'a4paper',
+}
 
 # The name of an image file (relative to this directory) to place at the top of
 # the title page.
 latex_logo = "media/agid-logo.png"
-
-# For "manual" documents, if this is true, then toplevel headings are parts,
-# not chapters.
-#latex_use_parts = False
 
 # If true, show page references after internal links.
 latex_show_pagerefs = False
