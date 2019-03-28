20c856b47adb7cba15bab9e0ed62bd73d0dc0fcc
Davide Porrovecchio <pdavide@users.noreply.github.com>
2019-03-28 10:24:29 +0100

    Add missing master_doc

--- conf.py
+++ conf.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# The master toctree document.	
+master_doc = 'index'
+
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
